497b6f20a37203dbfed295ed9bb72b3642b88806
marcos oliva <marcos.alois.oliva@gmail.com>
2018-03-17 20:18:02 -0700

    video 24 Using watch - scope/closure

--- main.js
+++ main.js
@@ -17,5 +17,16 @@ new Vue({
          return this.counter > 5 ? 'Greater 5' : 'Smaller than 5'
        }
        
+    },
+    watch: {
+    	counter: function(value) {
+    		// we set the vvue instance = vm
+    		// since this is closure 
+    		var vm = this;
+    		setTimeout(function(){
+              vm.counter = 0 ;
+    		} ,2000)
+
+    	}
     }
 })
